1551ee06634c5f1046e3c8a8127916d35c7f15db
David Larlet <larlet@gmail.com>
2010-05-27 12:25:06 +0200

    Adding imports for edit views

--- class_based_views/__init__.py
+++ class_based_views/__init__.py
@@ -2,5 +2,7 @@ from class_based_views.base import GenericView
 from class_based_views.list import ListView
 from class_based_views.detail import DetailView
 from class_based_views.dates import (ArchiveView, YearView, MonthView,
-                                         WeekView, DayView, TodayView,
-                                         DateDetailView)
+                                     WeekView, DayView, TodayView,
+                                     DateDetailView)
+from class_based_views.edit import (FormView, CreateView, UpdateView,
+                                    DeleteView)
